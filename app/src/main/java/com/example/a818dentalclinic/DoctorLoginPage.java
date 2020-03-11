987dbec64a3d32fac7657c4b2f70b9ffe5a41fbb
Jumac <juliusmacabulos26@gmail.com>
2020-03-11 14:21:06 +0800

    update

--- app/src/main/java/com/example/a818dentalclinic/DoctorLoginPage.java
+++ app/src/main/java/com/example/a818dentalclinic/DoctorLoginPage.java
@@ -1,13 +1,21 @@
 package com.example.a818dentalclinic;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.content.Intent;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.Objects;
 
 public class DoctorLoginPage extends AppCompatActivity {
 
@@ -22,12 +30,36 @@ public class DoctorLoginPage extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.doctor_login_page);
 
+        userID = findViewById(R.id.doctor_name);
+        pass = findViewById(R.id.doctor_pass);
+        fStore = FirebaseFirestore.getInstance();
+
         loginButton = findViewById(R.id.doctor_login);
+
         loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(DoctorLoginPage.this , DoctorAppointmentsSevenDays.class);
-                startActivity(intent);
+
+                String userName = userID.getText().toString().trim();
+                String password = pass.getText().toString().trim();
+
+                if (TextUtils.isEmpty(userName)) {
+                    userID.setError("Username is Required!");
+                    return;
+                }
+
+                if (TextUtils.isEmpty(password)) {
+                    pass.setError("Password is Required!");
+                    return;
+                }
+
+                if (password.length() < 6) {
+                    pass.setError("Password must be greater than 5 characters!");
+                    return;
+                }
+
+
+
             }
         });
 
@@ -39,9 +71,5 @@ public class DoctorLoginPage extends AppCompatActivity {
                 startActivity(intent);
             }
         });
-
-        userID = findViewById(R.id.doctor_name);
-        pass = findViewById(R.id.doctor_pass);
-        fStore = FirebaseFirestore.getInstance();
     }
 }
