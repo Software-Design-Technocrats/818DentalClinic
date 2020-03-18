914605442c65198165e47518911ce8fd3390b993
rainyearr <49300740+rainyearr@users.noreply.github.com>
2020-03-18 20:45:19 +0800

    New layout & Redesign

---
+++ app/src/main/java/com/example/a818dentalclinic/AddAppointmentFinish.java
@@ -0,0 +1,14 @@
+package com.example.a818dentalclinic;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AddAppointmentFinish extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_appointment_finish);
+    }
+}
