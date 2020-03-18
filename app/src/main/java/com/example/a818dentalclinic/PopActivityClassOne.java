f8c410582756121c801fe8722b20869c39728176
rainyearr <49300740+rainyearr@users.noreply.github.com>
2020-03-18 14:51:52 +0800

    Pop Up Added

--- app/src/main/java/com/example/a818dentalclinic/PopActivityClassOne.java
+++ app/src/main/java/com/example/a818dentalclinic/PopActivityClassOne.java
@@ -2,17 +2,33 @@ package com.example.a818dentalclinic;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
 import android.view.Gravity;
+import android.view.View;
 import android.view.WindowManager;
+import android.widget.Button;
 
 public class PopActivityClassOne extends AppCompatActivity {
+
+    Button date_time_picker;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pop_class_one);
+
+        date_time_picker = findViewById(R.id.button5);
+        date_time_picker.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(PopActivityClassOne.this, DateTimePicker.class);
+                startActivity(intent);
+            }
+        });
+
+
 
         DisplayMetrics dm = new DisplayMetrics();
         getWindowManager().getDefaultDisplay().getMetrics(dm);
